75f9ae77e2641f03108b9a0231d0434bfe68d6a7
Max <98maxekh@gafe.molndal.se>
2021-12-01 18:12:07 +0100

    Created Person.cs and relevant code. Installed "Microsoft.EntityFrameworkCore.SqlServer" and "Microsoft.EntityFrameworkCore.Tools"

---
+++ GenealogiProject/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenealogiProject.Models
+{
+    public class Person 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; } //VG
+        public string DateOfDeath { get; set; } //VG
+        public int Mother { get; set; }
+        public int Father { get; set; }      
+    }
+}
