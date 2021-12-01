0902d0d4453d7b363a708150cf4e45aefce644e1
Max <98maxekh@gafe.molndal.se>
2021-12-01 18:18:42 +0100

    Created FamilyContext.cs and added relevant code

---
+++ GenealogiProject/Database/FamilyContext.cs
@@ -0,0 +1,19 @@
+﻿using GenealogiProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenealogiProject.Database
+{
+    public class FamilyContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MEGenealogi;Trusted_Connection=True;");
+        }
+    }
+}
