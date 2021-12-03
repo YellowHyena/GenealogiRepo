923d1f99154726b7192dceb461ed6c9161409996
Max <98maxekh@gafe.molndal.se>
2021-12-03 15:49:45 +0100

    migrate och update gjorda

--- GenealogiProject/Migrations/20211203144538_kommerdetattfunka.Designer.cs
+++ GenealogiProject/Migrations/20211203144538_kommerdetattfunka.Designer.cs
@@ -3,6 +3,7 @@ using GenealogiProject.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GenealogiProject.Migrations
 {
     [DbContext(typeof(FamilyContext))]
-    partial class FamilyContextModelSnapshot : ModelSnapshot
+    [Migration("20211203144538_kommerdetattfunka")]
+    partial class kommerdetattfunka
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
