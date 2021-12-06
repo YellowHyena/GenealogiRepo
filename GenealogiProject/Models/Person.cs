483d9c02d8abaea65b253ec9f3094f4ea9df6d04
Max <98maxekh@gafe.molndal.se>
2021-12-06 10:26:13 +0100

    gave variables "empty" values to avoid them being null

--- GenealogiProject/Models/Person.cs
+++ GenealogiProject/Models/Person.cs
@@ -9,10 +9,10 @@ namespace GenealogiProject.Models
     public class Person 
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string LastName { get; set; }
-        public string DateOfBirth { get; set; } //VG
-        public string DateOfDeath { get; set; } //VG
+        public string Name { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string DateOfBirth { get; set; } = ""; //VG
+        public string DateOfDeath { get; set; } = ""; //VG
         public int Mother { get; set; }
         public int Father { get; set; }      
     }
